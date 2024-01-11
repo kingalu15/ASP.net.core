8b9888106cfd8f218e21e02350d43c6b00553170
kingalu15 <134364590+kingalu15@users.noreply.github.com>
2024-01-12 00:14:18 +0300

    ResourceFilter

---
+++ CRUDExample/Filters/ResourceFilters/FeatureDisabledResourceFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ResourceFilters
+{
+    public class FeatureDisabledResourceFilter : IAsyncResourceFilter
+    {
+        private readonly ILogger<FeatureDisabledResourceFilter> _logger;
+        private readonly bool _isFeatureEnabled = false;
+
+        public FeatureDisabledResourceFilter(ILogger<FeatureDisabledResourceFilter> logger,bool isFeatureEnabled=false)
+        {
+            _logger = logger;
+            _isFeatureEnabled = isFeatureEnabled;
+        }   
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            //To Do:before logic here
+            _logger.LogInformation("{FilterName}.{MethodName} method before", nameof(FeatureDisabledResourceFilter), nameof(OnResourceExecutionAsync));
+            if(_isFeatureEnabled == false)
+            {
+              //context.Result = new NotFoundResult();
+              context.Result = new StatusCodeResult(501);
+            }else
+            {
+                await next();
+            }
+         
+            //To Do:after logic here
+            _logger.LogInformation("{FilterName}.{MethodName} method after", nameof(FeatureDisabledResourceFilter), nameof(OnResourceExecutionAsync));  
+        }
+    }
+}
